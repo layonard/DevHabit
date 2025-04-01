7dcaacf73bf0b617dd4706951dd94f0b67c7c642
Leonardo Vargas <layonard@gmail.com>
2025-03-31 23:44:20 -0400

    Introduce Database and Habit model

---
+++ DevHabit.Api/Database/Configurations/HabitConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevHabit.Api.Database.Configurations;
+
+public sealed class HabitConfiguration : IEntityTypeConfiguration<Habit>
+{
+    public void Configure(EntityTypeBuilder<Habit> builder)
+    {
+        builder.HasKey(habit => habit.Id);
+
+        builder.Property(habit => habit.Id).HasMaxLength(500);
+        builder.Property(habit => habit.Name).HasMaxLength(100);
+        builder.Property(habit => habit.Description).HasMaxLength(500);
+
+        builder.OwnsOne(habit => habit.Frequency);
+        builder.OwnsOne(habit => habit.Target, targetBuilder =>
+        {
+            targetBuilder.Property(target => target.Unit).HasMaxLength(100);
+        });
+        builder.OwnsOne(habit => habit.Milestone);
+    }
+}
